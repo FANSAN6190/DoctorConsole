d05bf20224f59db5297a5178037d3415a3d5cd3f
FANSAN6190 <fanin.s.pbl@gmail.com>
2024-09-19 11:00:42 +0530

    stacked window

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtWidgets import QApplication, QMessageBox
+from PySide6.QtWidgets import QApplication, QMessageBox, QPushButton, QStackedWidget, QMainWindow
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import Qt, QFile
 
+class MainWindow(QMainWindow):
+    def __init__(self, ui_file_path):
+        super().__init__()
+        self.ui_file_path = ui_file_path
+        self.load_ui()
+        self.setup_ui()
+
+    def load_ui(self):
+        ui_file = QFile(self.ui_file_path)
+        if not ui_file.open(QFile.ReadOnly):
+            raise IOError(f"Cannot open {self.ui_file_path}: {ui_file.errorString()}")
+
+        loader = QUiLoader()
+        self.window = loader.load(ui_file, self)
+        ui_file.close()
+
+        if self.window is None:
+            raise RuntimeError(f"Failed to load UI file: {self.ui_file_path}")
+
+    def setup_ui(self):
+        # Find the QStackedWidget
+        self.stacked_widget = self.window.findChild(QStackedWidget, "stackedWidget")
+
+        # Find the buttons
+        self.button_page1 = self.window.findChild(QPushButton, "medHistory")
+        self.button_page2 = self.window.findChild(QPushButton, "newPriscrip")
+
+        # Connect buttons to change pages
+        self.button_page1.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.button_page2.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
+
+        self.window.show()
+
 def main():
-    # Set the attribute before creating the QApplication instance
     QApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication(sys.argv)
-    
-    ui_file_path = "mainwindow.ui"
-    
+
     try:
-        ui_file = QFile(ui_file_path)
-        if not ui_file.open(QFile.ReadOnly):
-            raise IOError(f"Cannot open {ui_file_path}: {ui_file.errorString()}")
-        
-        loader = QUiLoader()
-        window = loader.load(ui_file)
-        ui_file.close()
-        
-        if window is None:
-            raise RuntimeError(f"Failed to load UI file: {ui_file_path}")
-        
-        window.show()
+        MainWindow("mainwindow.ui")
         sys.exit(app.exec())
-    
+
     except Exception as e:
         QMessageBox.critical(None, "Error", str(e))
         sys.exit(1)
---
+++ ui_mainwindow.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QGroupBox, QLabel, QLineEdit,
+    QMainWindow, QMenu, QMenuBar, QPushButton,
+    QSizePolicy, QStackedWidget, QStatusBar, QVBoxLayout,
+    QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1920, 1080)
+        self.actionPriscription = QAction(MainWindow)
+        self.actionPriscription.setObjectName(u"actionPriscription")
+        self.actionPatient_Details = QAction(MainWindow)
+        self.actionPatient_Details.setObjectName(u"actionPatient_Details")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalWidget = QWidget(self.centralwidget)
+        self.verticalWidget.setObjectName(u"verticalWidget")
+        self.verticalWidget.setGeometry(QRect(0, 0, 381, 1031))
+        self.verticalWidget.setStyleSheet(u"border-right: 2px solid black;")
+        self.patientDetails = QGroupBox(self.verticalWidget)
+        self.patientDetails.setObjectName(u"patientDetails")
+        self.patientDetails.setGeometry(QRect(10, 350, 361, 621))
+        self.verticalLayout_2 = QVBoxLayout(self.patientDetails)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.imageSection = QWidget(self.patientDetails)
+        self.imageSection.setObjectName(u"imageSection")
+        self.imageSection.setStyleSheet(u"border-right:none;")
+
+        self.verticalLayout_2.addWidget(self.imageSection)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.textDetails = QLabel(self.patientDetails)
+        self.textDetails.setObjectName(u"textDetails")
+
+        self.verticalLayout.addWidget(self.textDetails)
+
+        self.medHistory = QPushButton(self.patientDetails)
+        self.medHistory.setObjectName(u"medHistory")
+        self.medHistory.setStyleSheet(u"margin: 20px")
+
+        self.verticalLayout.addWidget(self.medHistory)
+
+        self.newPriscrip = QPushButton(self.patientDetails)
+        self.newPriscrip.setObjectName(u"newPriscrip")
+        self.newPriscrip.setStyleSheet(u"margin: 20px")
+
+        self.verticalLayout.addWidget(self.newPriscrip)
+
+
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.searchInput = QLineEdit(self.verticalWidget)
+        self.searchInput.setObjectName(u"searchInput")
+        self.searchInput.setGeometry(QRect(10, 50, 261, 31))
+        self.searchPatient = QPushButton(self.verticalWidget)
+        self.searchPatient.setObjectName(u"searchPatient")
+        self.searchPatient.setGeometry(QRect(280, 50, 89, 31))
+        self.searchLabel = QLabel(self.verticalWidget)
+        self.searchLabel.setObjectName(u"searchLabel")
+        self.searchLabel.setGeometry(QRect(10, 20, 251, 21))
+        self.stackedWidget = QStackedWidget(self.centralwidget)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.stackedWidget.setGeometry(QRect(390, 10, 1511, 1021))
+        self.medicalHistory = QWidget()
+        self.medicalHistory.setObjectName(u"medicalHistory")
+        self.widget_2 = QWidget(self.medicalHistory)
+        self.widget_2.setObjectName(u"widget_2")
+        self.widget_2.setGeometry(QRect(150, 10, 1231, 961))
+        self.label_3 = QLabel(self.widget_2)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(560, 30, 111, 31))
+        self.stackedWidget.addWidget(self.medicalHistory)
+        self.newPriscription = QWidget()
+        self.newPriscription.setObjectName(u"newPriscription")
+        self.widget_3 = QWidget(self.newPriscription)
+        self.widget_3.setObjectName(u"widget_3")
+        self.widget_3.setGeometry(QRect(150, 10, 1231, 961))
+        self.label_4 = QLabel(self.widget_3)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(560, 30, 111, 31))
+        self.stackedWidget.addWidget(self.newPriscription)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 1920, 22))
+        self.menuHome = QMenu(self.menubar)
+        self.menuHome.setObjectName(u"menuHome")
+        self.menuNew = QMenu(self.menubar)
+        self.menuNew.setObjectName(u"menuNew")
+        self.menuGet = QMenu(self.menubar)
+        self.menuGet.setObjectName(u"menuGet")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuHome.menuAction())
+        self.menubar.addAction(self.menuNew.menuAction())
+        self.menubar.addAction(self.menuGet.menuAction())
+        self.menuNew.addAction(self.actionPriscription)
+        self.menuGet.addAction(self.actionPatient_Details)
+
+        self.retranslateUi(MainWindow)
+
+        self.stackedWidget.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionPriscription.setText(QCoreApplication.translate("MainWindow", u"Priscription", None))
+        self.actionPatient_Details.setText(QCoreApplication.translate("MainWindow", u"Patient Details", None))
+        self.patientDetails.setTitle(QCoreApplication.translate("MainWindow", u"Patient Details", None))
+        self.textDetails.setText(QCoreApplication.translate("MainWindow", u"Name:", None))
+        self.medHistory.setText(QCoreApplication.translate("MainWindow", u"View Medical History", None))
+        self.newPriscrip.setText(QCoreApplication.translate("MainWindow", u"Create New Prescription", None))
+        self.searchPatient.setText(QCoreApplication.translate("MainWindow", u"Search", None))
+        self.searchLabel.setText(QCoreApplication.translate("MainWindow", u"Search Patient by Email ID", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Medical History", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"New Priscription", None))
+        self.menuHome.setTitle(QCoreApplication.translate("MainWindow", u"Home", None))
+        self.menuNew.setTitle(QCoreApplication.translate("MainWindow", u"New", None))
+        self.menuGet.setTitle(QCoreApplication.translate("MainWindow", u"Get", None))
+    # retranslateUi
+
